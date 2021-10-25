2f03857ce1cfc1671f29eba201055fe44dafd327
shubhanshukeshari <shubhanshu.keshari@innovaccer.com>
2021-10-25 13:47:38 +0530

    add one more test case

--- test_calc.py
+++ test_calc.py
@@ -6,5 +6,8 @@ class TestCalc(unittest.TestCase):
     def test_add(self):
         result= main.add(10,5)
         self.assertEqual(15,result)
+    def test_sub(self):
+        result = main.substract(10,5)
+        self.assertEqual(5,result)
 if __name__=='__main__':
     unittest.main()
